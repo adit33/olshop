1e5c62c15355d0563874747704ce9166d1c3ba67
adit33 <aditpastrana@gmail.com>
2017-10-06 16:19:22 +0700

    add table

--- resources/views/frontend/product/cart.blade.php
+++ resources/views/frontend/product/cart.blade.php
@@ -1,7 +1,25 @@
 @extends('frontend.layout.master')
 
 @section('content')
- @foreach($carts as $cart)
-  {!! $cart->name !!}
- @endforeach
+  
+  
+
+  <table class="table is-bordered is-striped is-narrow is-fullwidth">
+    <thead>
+      <th>Nama</th>
+      <th>Jumlah</th>
+      <th>Harga</th>
+      <th>Action</th>
+    </thead>
+    <tbody>
+     @foreach($carts as $cart)
+      <tr>
+        <td>{!! $cart->name !!}</td>
+        <td>{!! $cart->qty !!}</td>
+        <td>{!! $cart->price !!}</td>
+        <td>{!! $cart->name !!}</td>
+      </tr> 
+     @endforeach
+    </tbody>
+  </table>
 @endsection
